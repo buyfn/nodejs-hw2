48a049950896b6d9f52aff80b03913cc020a73c2
ihnat piatrenka <piatri01@kits.kfplc.com>
2023-01-26 19:31:53 +0100

    update config

--- knexfile.js
+++ knexfile.js
@@ -1,45 +1,52 @@
-// Update with your config settings.
+import 'dotenv/config.js';
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 export default {
-    development: {
-        client: 'postgresql',
-        connection: {
-            database: 'users'
-        }
-    },
-
-    staging: {
-        client: 'postgresql',
-        connection: {
-            database: 'users',
-            user:     'username',
-            password: 'password'
-        },
-        pool: {
-            min: 2,
-            max: 10
-        },
-        migrations: {
-            tableName: 'knex_migrations'
-        }
-    },
-
-    production: {
-        client: 'postgresql',
-        connection: {
-            database: 'users',
-            user:     'username',
-            password: 'password'
-        },
-        pool: {
-            min: 2,
-            max: 10
-        },
-        migrations: {
-            tableName: 'knex_migrations'
-        }
+  development: {
+    client: 'postgresql',
+    connection: {
+      host: process.env.POSTGRESQL_HOST,
+      port: process.env.POSTGRESQL_PORT,
+      database: process.env.POSTGRESQL_DB,
+      user: process.env.POSTGRESQL_USER,
+      password: process.env.POSTGRESQL_PASSWORD,
+      ssl: {
+        rejectUnauthorized: false
+      }
     }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'users',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'users',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
 };
