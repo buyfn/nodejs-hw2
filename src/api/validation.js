0fef3e9be4af68f5ef50cb6012585efc75af89c6
ignat <buyfng@gmail.com>
2023-01-31 17:19:35 +0100

    comment out test job

--- src/api/validation.js
+++ src/api/validation.js
@@ -11,7 +11,7 @@ export const groupSchema = Joi.object({
     name: Joi.string().required(),
     permissions: Joi.array().items(Joi.string().valid(
         'READ', 'WRITE', 'DELETE', 'SHARE', 'UPLOAD_FILES'
-    ))
+    )).required()
 });
 
 const errorResponse = (schemaErrors) => {
