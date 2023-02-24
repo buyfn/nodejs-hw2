45cb45a50e20ed14ffa3cfc1c8d57f06960f5e9a
ignat <buyfng@gmail.com>
2023-02-24 18:33:18 +0100

    Add error logging

--- src/api/groups.js
+++ src/api/groups.js
@@ -1,5 +1,6 @@
 import express from 'express';
 
+import { logger } from '../logger.js';
 import { configureUsersService } from '../config.js';
 import { groupSchema, validateBody } from './validation.js';
 
@@ -8,11 +9,16 @@ const usersService = configureUsersService();
 
 groupsRouter.route('/')
     .post(validateBody(groupSchema), async (req, res, next) => {
+        const group = req.body;
         try {
-            const group = req.body;
             const groupData = await usersService.addGroup(group);
             res.json(groupData);
         } catch (error) {
+            logger.error({
+                method: 'addGroup',
+                args: [group],
+                error: error.message
+            });
             return next(error);
         }
     })
@@ -21,12 +27,18 @@ groupsRouter.route('/')
             const groups = await usersService.getAllGroups();
             res.json(groups);
         } catch (error) {
+            logger.error({
+                method: 'getAllGroups',
+                args: [],
+                error: error.message
+            });
             return next(error);
         }
     });
 
 groupsRouter.route('/:id')
     .get(async (req, res, next) => {
+        const groupId = req.params.id;
         try {
             const group = await usersService.getGroup(req.params.id);
             if (group) {
@@ -35,37 +47,59 @@ groupsRouter.route('/:id')
                 res.status(404).json({ message: 'Group not found' });
             }
         } catch (error) {
+            logger.error({
+                method: 'getGroup',
+                args: [groupId],
+                error: error.message
+            });
             return next(error);
         }
     })
     .patch(validateBody(groupSchema), async (req, res, next) => {
+        const groupData = req.body;
+        const groupId = req.params.id;
         try {
-            const groupData = req.body;
             const updatedGroup = await usersService.updateGroup(
-                req.params.id,
+                groupId,
                 groupData
             );
             res.json(updatedGroup);
         } catch (error) {
+            logger.error({
+                method: 'updateGroup',
+                args: [groupId, groupData],
+                error: error.message
+            });
             return next(error);
         }
     })
     .delete(async (req, res, next) => {
+        const groupId = req.params.id;
         try {
-            await usersService.deleteGroup(req.params.id);
+            await usersService.deleteGroup(groupId);
             res.sendStatus(204);
         } catch (error) {
+            logger.error({
+                method: 'deleteGroup',
+                args: [groupId],
+                error: error.message
+            });
             return next(error);
         }
     })
     .post(async (req, res, next) => {
+        const users = req.body;
+        const groupId = req.params.id;
         try {
-            const users = req.body;
-            const groupId = req.params.id;
             const addedUsers = await usersService
                 .addUsersToGroup(groupId, users);
             res.json({ addedUsers });
         } catch (error) {
+            logger.error({
+                method: 'addUsersToGroup',
+                args: [groupId, users],
+                error: error.message
+            });
             return next(error);
         }
     });
